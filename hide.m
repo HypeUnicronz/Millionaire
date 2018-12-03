42c0754a0b3ee408832e567cadeff2ccf465c796
dancon40 <44585225+dancon40@users.noreply.github.com>
2018-12-03 13:53:33 -0500

    Add files via upload

--- hide.m
+++ hide.m
@@ -22,7 +22,7 @@ function varargout = hide(varargin)
 
 % Edit the above text to modify the response to help hide
 
-% Last Modified by GUIDE v2.5 08-Nov-2018 16:09:05
+% Last Modified by GUIDE v2.5 01-Dec-2018 20:32:05
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -79,6 +79,7 @@ set(handles.pushbutton1,'visible','off')
 set(handles.text9,'visible','off')
 %Switch to the Main Game
 set(handles.Title,'visible','off')
+set(handles.lifelines,'visible','on')
 set(handles.ans_A,'visible','on')
 set(handles.ans_B,'visible','on')
 set(handles.ans_C,'visible','on')
@@ -110,6 +111,12 @@ q = handles.cntr.String;
 switch q
     case '1'
         %Wrong Answer
+        pause(5);
+        set(handles.ans_A,'BackgroundColor','[1,0,0]')
+        set(handles.ans_B,'BackgroundColor','[0,1,0]')
+        set(handles.ans_C,'BackgroundColor','[1,0,0]')
+        set(handles.ans_D,'BackgroundColor','[1,0,0]')
+        pause(2);
         set(handles.text9,'visible','on')
         set(handles.text9,'String','Game Over')
         set(handles.Title,'visible','on')
@@ -126,12 +133,30 @@ switch q
         set(handles.pushbutton1,'String','Restart')
     case '2'
         [q,a1,a2,a3,a4,qnum] = retr(3);
+         pause(5);
+        set(handles.ans_A,'BackgroundColor','[0,1,0]')
+        set(handles.ans_B,'BackgroundColor','[1,0,0]')
+        set(handles.ans_C,'BackgroundColor','[1,0,0]')
+        set(handles.ans_D,'BackgroundColor','[1,0,0]')
+        pause(2);
         set(handles.cntr,'String',qnum)
         set(handles.Questions,'String',q)
         set(handles.txtans_A,'String',a1)
         set(handles.txtans_B,'String',a2)
         set(handles.txtans_C,'String',a3)
         set(handles.txtans_D,'String',a4)
+                set(handles.ans_A,'visible','on')
+        set(handles.ans_B,'visible','on')
+        set(handles.ans_C,'visible','on')
+        set(handles.ans_D,'visible','on')
+        set(handles.txtans_A,'visible','on')
+        set(handles.txtans_B,'visible','on')
+        set(handles.txtans_C,'visible','on')
+        set(handles.txtans_D,'visible','on')
+             set(handles.percentage1,'visible','off')
+        set(handles.percentage2,'visible','off')
+        set(handles.percentage3,'visible','off')
+        set(handles.percentage4,'visible','off')
     case '3'
                 set(handles.text9,'visible','on')
         set(handles.text9,'String','Game Over')
@@ -267,6 +292,18 @@ switch q
         set(handles.txtans_B,'String',a2)
         set(handles.txtans_C,'String',a3)
         set(handles.txtans_D,'String',a4)
+                set(handles.ans_A,'visible','on')
+        set(handles.ans_B,'visible','on')
+        set(handles.ans_C,'visible','on')
+        set(handles.ans_D,'visible','on')
+        set(handles.txtans_A,'visible','on')
+        set(handles.txtans_B,'visible','on')
+        set(handles.txtans_C,'visible','on')
+        set(handles.txtans_D,'visible','on')
+                set(handles.percentage1,'visible','off')
+        set(handles.percentage2,'visible','off')
+        set(handles.percentage3,'visible','off')
+        set(handles.percentage4,'visible','off')
     case '2'
                 set(handles.text9,'visible','on')
         set(handles.text9,'String','Game Over')
@@ -290,6 +327,18 @@ switch q
         set(handles.txtans_B,'String',a2)
         set(handles.txtans_C,'String',a3)
         set(handles.txtans_D,'String',a4)
+                set(handles.ans_A,'visible','on')
+        set(handles.ans_B,'visible','on')
+        set(handles.ans_C,'visible','on')
+        set(handles.ans_D,'visible','on')
+        set(handles.txtans_A,'visible','on')
+        set(handles.txtans_B,'visible','on')
+        set(handles.txtans_C,'visible','on')
+        set(handles.txtans_D,'visible','on')
+                set(handles.percentage1,'visible','off')
+        set(handles.percentage2,'visible','off')
+        set(handles.percentage3,'visible','off')
+        set(handles.percentage4,'visible','off')
     case '4'
         [q,a1,a2,a3,a4,qnum] = retr(5);
         set(handles.cntr,'String',qnum)
@@ -298,6 +347,18 @@ switch q
         set(handles.txtans_B,'String',a2)
         set(handles.txtans_C,'String',a3)
         set(handles.txtans_D,'String',a4)
+                set(handles.ans_A,'visible','on')
+        set(handles.ans_B,'visible','on')
+        set(handles.ans_C,'visible','on')
+        set(handles.ans_D,'visible','on')
+        set(handles.txtans_A,'visible','on')
+        set(handles.txtans_B,'visible','on')
+        set(handles.txtans_C,'visible','on')
+        set(handles.txtans_D,'visible','on')
+                set(handles.percentage1,'visible','off')
+        set(handles.percentage2,'visible','off')
+        set(handles.percentage3,'visible','off')
+        set(handles.percentage4,'visible','off')
     case '5'
                 set(handles.text9,'visible','on')
         set(handles.text9,'String','Game Over')
@@ -321,6 +382,18 @@ switch q
         set(handles.txtans_B,'String',a2)
         set(handles.txtans_C,'String',a3)
         set(handles.txtans_D,'String',a4)
+                set(handles.ans_A,'visible','on')
+        set(handles.ans_B,'visible','on')
+        set(handles.ans_C,'visible','on')
+        set(handles.ans_D,'visible','on')
+        set(handles.txtans_A,'visible','on')
+        set(handles.txtans_B,'visible','on')
+        set(handles.txtans_C,'visible','on')
+        set(handles.txtans_D,'visible','on')
+                set(handles.percentage1,'visible','off')
+        set(handles.percentage2,'visible','off')
+        set(handles.percentage3,'visible','off')
+        set(handles.percentage4,'visible','off')
     case '7'
                 set(handles.text9,'visible','on')
         set(handles.text9,'String','Game Over')
@@ -504,6 +577,18 @@ switch q
         set(handles.txtans_B,'String',a2)
         set(handles.txtans_C,'String',a3)
         set(handles.txtans_D,'String',a4)
+                set(handles.ans_A,'visible','on')
+        set(handles.ans_B,'visible','on')
+        set(handles.ans_C,'visible','on')
+        set(handles.ans_D,'visible','on')
+        set(handles.txtans_A,'visible','on')
+        set(handles.txtans_B,'visible','on')
+        set(handles.txtans_C,'visible','on')
+        set(handles.txtans_D,'visible','on')
+                set(handles.percentage1,'visible','off')
+        set(handles.percentage2,'visible','off')
+        set(handles.percentage3,'visible','off')
+        set(handles.percentage4,'visible','off')
     case '9'
                 set(handles.text9,'visible','on')
         set(handles.text9,'String','Game Over')
@@ -612,6 +697,18 @@ switch q
         set(handles.txtans_B,'String',a2)
         set(handles.txtans_C,'String',a3)
         set(handles.txtans_D,'String',a4)
+                set(handles.ans_A,'visible','on')
+        set(handles.ans_B,'visible','on')
+        set(handles.ans_C,'visible','on')
+        set(handles.ans_D,'visible','on')
+        set(handles.txtans_A,'visible','on')
+        set(handles.txtans_B,'visible','on')
+        set(handles.txtans_C,'visible','on')
+        set(handles.txtans_D,'visible','on')
+                set(handles.percentage1,'visible','off')
+        set(handles.percentage2,'visible','off')
+        set(handles.percentage3,'visible','off')
+        set(handles.percentage4,'visible','off')
     case '6'
                 set(handles.text9,'visible','on')
         set(handles.text9,'String','Game Over')
@@ -635,6 +732,18 @@ switch q
         set(handles.txtans_B,'String',a2)
         set(handles.txtans_C,'String',a3)
         set(handles.txtans_D,'String',a4)
+                set(handles.ans_A,'visible','on')
+        set(handles.ans_B,'visible','on')
+        set(handles.ans_C,'visible','on')
+        set(handles.ans_D,'visible','on')
+        set(handles.txtans_A,'visible','on')
+        set(handles.txtans_B,'visible','on')
+        set(handles.txtans_C,'visible','on')
+        set(handles.txtans_D,'visible','on')
+                set(handles.percentage1,'visible','off')
+        set(handles.percentage2,'visible','off')
+        set(handles.percentage3,'visible','off')
+        set(handles.percentage4,'visible','off')
     case '8'
                 set(handles.text9,'visible','on')
         set(handles.text9,'String','Game Over')
@@ -658,6 +767,18 @@ switch q
         set(handles.txtans_B,'String',a2)
         set(handles.txtans_C,'String',a3)
         set(handles.txtans_D,'String',a4)
+                set(handles.ans_A,'visible','on')
+        set(handles.ans_B,'visible','on')
+        set(handles.ans_C,'visible','on')
+        set(handles.ans_D,'visible','on')
+        set(handles.txtans_A,'visible','on')
+        set(handles.txtans_B,'visible','on')
+        set(handles.txtans_C,'visible','on')
+        set(handles.txtans_D,'visible','on')
+                set(handles.percentage1,'visible','off')
+        set(handles.percentage2,'visible','off')
+        set(handles.percentage3,'visible','off')
+        set(handles.percentage4,'visible','off')
     case '10'
                 set(handles.text9,'visible','on')
         set(handles.text9,'String','Game Over')
@@ -673,7 +794,187 @@ switch q
         set(handles.Questions,'visible','off')
         set(handles.pushbutton1,'visible','on')
         set(handles.pushbutton1,'String','Restart')
+        
 end
 % hObject    handle to ans_D (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on button press in phoneTheGenius.
+function phoneTheGenius_Callback(hObject, eventdata, handles)
+% hObject    handle to phoneTheGenius (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.phoneTheGenius,'visible','off')
+q = handles.cntr.String;
+q=str2double(q);
+ [q,a1,a2,a3,a4,qnum] = retr((q+1));
+        set(handles.cntr,'String',qnum)
+        set(handles.Questions,'String',q)
+        set(handles.txtans_A,'String',a1)
+        set(handles.txtans_B,'String',a2)
+        set(handles.txtans_C,'String',a3)
+        set(handles.txtans_D,'String',a4)
+% --- Executes on button press in askThePeople.
+function askThePeople_Callback(hObject, eventdata, handles)
+% hObject    handle to askThePeople (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.askThePeople,'visible','off')
+q = handles.cntr.String;
+switch q
+    case '1'
+        set(handles.percentage1,'visible','on')
+        set(handles.percentage2,'visible','on')
+        set(handles.percentage3,'visible','on')
+        set(handles.percentage4,'visible','on')
+        set(handles.percentage1,'String','15%')
+        set(handles.percentage2,'String','65%')
+        set(handles.percentage3,'String','10%')
+        set(handles.percentage4,'String','10%')
+    case '2'
+        set(handles.percentage1,'visible','on')
+        set(handles.percentage2,'visible','on')
+        set(handles.percentage3,'visible','on')
+        set(handles.percentage4,'visible','on')
+        set(handles.percentage1,'String','55%')
+        set(handles.percentage2,'String','25%')
+        set(handles.percentage3,'String','0%')
+        set(handles.percentage4,'String','20%')
+    case '3'
+       set(handles.percentage1,'visible','on')
+        set(handles.percentage2,'visible','on')
+        set(handles.percentage3,'visible','on')
+        set(handles.percentage4,'visible','on')
+        set(handles.percentage1,'String','15%')
+        set(handles.percentage2,'String','75%')
+        set(handles.percentage3,'String','0%')
+        set(handles.percentage4,'String','10%')
+    case '4'
+       set(handles.percentage1,'visible','on')
+        set(handles.percentage2,'visible','on')
+        set(handles.percentage3,'visible','on')
+        set(handles.percentage4,'visible','on')
+        set(handles.percentage1,'String','15%')
+        set(handles.percentage2,'String','65%')
+        set(handles.percentage3,'String','10%')
+        set(handles.percentage4,'String','10%')
+    case '5'
+        set(handles.percentage1,'visible','on')
+        set(handles.percentage2,'visible','on')
+        set(handles.percentage3,'visible','on')
+        set(handles.percentage4,'visible','on')
+        set(handles.percentage1,'String','5%')
+        set(handles.percentage2,'String','5%')
+        set(handles.percentage3,'String','10%')
+        set(handles.percentage4,'String','80%')
+    case '6'
+        set(handles.percentage1,'visible','on')
+        set(handles.percentage2,'visible','on')
+        set(handles.percentage3,'visible','on')
+        set(handles.percentage4,'visible','on')
+        set(handles.percentage1,'String','15%')
+        set(handles.percentage2,'String','65%')
+        set(handles.percentage3,'String','10%')
+        set(handles.percentage4,'String','10%')
+    case '7'
+        set(handles.percentage1,'visible','on')
+        set(handles.percentage2,'visible','on')
+        set(handles.percentage3,'visible','on')
+        set(handles.percentage4,'visible','on')
+        set(handles.percentage1,'String','5%')
+        set(handles.percentage2,'String','15%')
+        set(handles.percentage3,'String','10%')
+        set(handles.percentage4,'String','70%')
+    case '8'
+        set(handles.percentage1,'visible','on')
+        set(handles.percentage2,'visible','on')
+        set(handles.percentage3,'visible','on')
+        set(handles.percentage4,'visible','on')
+        set(handles.percentage1,'String','15%')
+        set(handles.percentage2,'String','15%')
+        set(handles.percentage3,'String','60%')
+        set(handles.percentage4,'String','10%')
+    case '9'
+        set(handles.percentage1,'visible','on')
+        set(handles.percentage2,'visible','on')
+        set(handles.percentage3,'visible','on')
+        set(handles.percentage4,'visible','on')
+        set(handles.percentage1,'String','15%')
+        set(handles.percentage2,'String','25%')
+        set(handles.percentage3,'String','10%')
+        set(handles.percentage4,'String','50%')
+    case '10'
+        set(handles.percentage1,'visible','on')
+        set(handles.percentage2,'visible','on')
+        set(handles.percentage3,'visible','on')
+        set(handles.percentage4,'visible','on')
+        set(handles.percentage1,'String','15%')
+        set(handles.percentage2,'String','5%')
+        set(handles.percentage3,'String','10%')
+        set(handles.percentage4,'String','70%')
+end
+
+
+
+% --- Executes on button press in fifty_fifty.
+function fifty_fifty_Callback(hObject, eventdata, handles)
+% hObject    handle to fifty_fifty (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+set(handles.fifty_fifty,'visible','off')
+q = handles.cntr.String;
+switch q
+    case '1'
+        set(handles.ans_A,'visible','off')
+        set(handles.ans_D,'visible','off')
+        set(handles.txtans_A,'visible','off')
+        set(handles.txtans_D,'visible','off')
+    case '2'
+        set(handles.ans_C,'visible','off')
+        set(handles.ans_D,'visible','off')
+        set(handles.txtans_C,'visible','off')
+        set(handles.txtans_D,'visible','off')
+    case '3'
+        set(handles.ans_A,'visible','off')
+        set(handles.ans_D,'visible','off')
+        set(handles.txtans_A,'visible','off')
+        set(handles.txtans_D,'visible','off')
+    case '4'
+        set(handles.ans_A,'visible','off')
+        set(handles.ans_C,'visible','off')
+        set(handles.txtans_A,'visible','off')
+        set(handles.txtans_C,'visible','off')
+    case '5'
+        set(handles.ans_A,'visible','off')
+        set(handles.ans_B,'visible','off')
+        set(handles.txtans_A,'visible','off')
+        set(handles.txtans_B,'visible','off')
+    case '6'
+        set(handles.ans_A,'visible','off')
+        set(handles.ans_D,'visible','off')
+        set(handles.txtans_A,'visible','off')
+        set(handles.txtans_D,'visible','off')
+    case '7'
+        set(handles.ans_A,'visible','off')
+        set(handles.ans_B,'visible','off')
+        set(handles.txtans_A,'visible','off')
+        set(handles.txtans_B,'visible','off')
+    case '8'
+        set(handles.ans_A,'visible','off')
+        set(handles.ans_B,'visible','off')
+        set(handles.txtans_A,'visible','off')
+        set(handles.txtans_B,'visible','off')
+    case '9'
+        set(handles.ans_A,'visible','off')
+        set(handles.ans_C,'visible','off')
+        set(handles.txtans_A,'visible','off')
+        set(handles.txtans_C,'visible','off')
+    case '10'
+        set(handles.ans_A,'visible','off')
+        set(handles.ans_B,'visible','off')
+        set(handles.txtans_A,'visible','off')
+        set(handles.txtans_B,'visible','off')
+end
+  
